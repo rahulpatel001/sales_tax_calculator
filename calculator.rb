d5b1524c49917147701fb99f82dfe42aa92c50e6
RahulPatel <rpatel@grepruby.com>
2017-11-14 18:50:55 +0530

    refactor code for calculator

---
+++ calculator.rb
@@ -0,0 +1,53 @@
+class SalesTaxes
+
+  TAX_EXCEPT_ITEMS = ["chocolates","book","pills","chocolate"]
+
+  def initialize
+    user_inputs = user_input
+    puts "Invoice Output"
+    get_output(user_inputs)
+  end
+
+  def user_input
+    array = []
+    puts "please enter number of inputs"
+    num = gets.to_i
+    puts "enter inputs"
+    (1..num).each do |val|
+      array << gets.chomp
+    end
+    array
+  end
+ 
+  def get_output(user_input_ary)
+    total_tax = 0
+    total_price = 0
+    user_input_ary.each do |item|
+      qty = item.split()[0].to_i
+      price = item.split()[-1].to_f * qty.to_f
+      product = item.split(" at ")[0].delete("/0-9/").strip
+      tax = tax_calculation(price, product)
+      total_tax += tax
+      tax_price = (price.to_f + tax)
+      total_price += tax_price
+      puts "#{qty} #{product}: #{tax_price.round(2)}"
+    end
+    puts "Sales Tax: #{total_tax.round(2)}"
+    puts "Total: #{total_price.round(2)}"
+  end
+ 
+  def tax_calculation(price, product)
+    tax = 0
+    tax_exclude = []
+    tax_exclude = product.split(" ") & TAX_EXCEPT_ITEMS
+    if product.include?('imported')
+      tax = price.to_f * 0.05
+    end
+    if tax_exclude.count != 1
+      tax = (price.to_f * 0.10) + tax.to_f
+    end
+    return tax
+  end
+end
+
+SalesTaxes.new
